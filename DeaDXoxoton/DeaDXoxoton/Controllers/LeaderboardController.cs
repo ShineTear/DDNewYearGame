92e25bbc1ac7593770a9123d49a6e27ba400f201
pestova.m <pestova.m@skbkontur.ru>
2022-12-01 13:57:20 +0500

    new project

---
+++ DeaDXoxoton/DeaDXoxoton/Controllers/LeaderboardController.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using DeaDXoxoton.Entity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DeaDXoxoton.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class LeaderboardController : ControllerBase
+{
+    [HttpGet(Name = "ShowScore")]
+    public List<Leaderboard> ShowScoreOnGame(string gameName)
+    {
+        var leaderboard = LoadLeaderboard();
+        return leaderboard.Where(x => x.GameName.Equals(gameName)).ToList();
+    }
+    
+    private List<Leaderboard> LoadLeaderboard()
+    {
+        var leaderboard = new List<Leaderboard>();
+        using (StreamReader r = new StreamReader("Score.json"))
+        {
+            string json = r.ReadToEnd();
+            leaderboard = JsonSerializer.Deserialize<List<Leaderboard>>(json);
+        }
+
+        return leaderboard;
+    }
+}
---
+++ DeaDXoxoton/DeaDXoxoton/Entity/Leaderboard.cs
@@ -0,0 +1,8 @@
+﻿namespace DeaDXoxoton.Entity;
+
+public class Leaderboard
+{
+    public string GameName { get; set; }
+    public int Score { get; set; }
+    public string Name { get; set; }
+}
