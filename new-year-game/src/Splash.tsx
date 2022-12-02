513d2c0562ba614210e89828439c34278ec8fc7d
arina_ra <arina_ra@skbkontur.ru>
2022-12-02 14:50:16 +0500

    fix

--- new-year-game/src/Splash.tsx
+++ new-year-game/src/Splash.tsx
@@ -8,8 +8,9 @@ export function Splash() {
             <h2>Дед Хо-Хо-тун</h2>
             <div>Хо-Хо! Дед Мороз!</div>
             <div>Беги быстрее от 2022, успей выкинуть все лишнее из своих карманчиков, пока не наступил 2023.</div>
-            <div>Самый быстрый Дед мороз получает приз (Хо-Хо-Хо!)</div>
+            <div>Самый быстрый Дед Mороз получает приз (Хо-Хо-Хо!)</div>
             <br/>
+        
             <RouterLink to="/Game">Играть</RouterLink>
             <RouterLink to="/Leaderboard">Доска почета</RouterLink>
             <div className="cropped"><img src={background}/></div>
